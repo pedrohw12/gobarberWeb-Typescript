f251ee60f464469f82c0ca42f15a6781ce561276
Pedro Wanderley <peu-zico>
2020-05-24 20:12:40 -0300

    singUp page <form>

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
+import { Form } from '@unform/web';
 
 import logoImg from "../../assets/logo.svg";
 
@@ -8,13 +9,18 @@ import Button from "../../components/Button";
 
 import { Container, Content, Background } from "./styles";
 
-const SignUp: React.FC = () => (
-  <Container>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
     <Background />
     <Content>
       <img src={logoImg} alt="GoBarber" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h1>Faça seu cadastro</h1>
 
         <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -27,7 +33,7 @@ const SignUp: React.FC = () => (
         />
 
         <Button type="submit">Cadastrar</Button>
-      </form>
+      </Form>
 
       <a href="login">
         <FiArrowLeft />
@@ -35,6 +41,7 @@ const SignUp: React.FC = () => (
       </a>
     </Content>
   </Container>
-);
+  );
+};
 
 export default SignUp;
