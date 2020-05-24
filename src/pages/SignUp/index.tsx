42ec295b9fe8f2033cd96a1b5e18e6acf3228b7c
Pedro Wanderley <peu-zico>
2020-05-24 20:59:52 -0300

    validating signUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
-import { Form } from '@unform/web';
+import { Form } from "@unform/web";
+import * as Yup from "yup";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -10,37 +11,51 @@ import Button from "../../components/Button";
 import { Container, Content, Background } from "./styles";
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome obrigatório"),
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().min(6, "No mínimo 6 digitos"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false, // vai retornar todos os erros e não apenas o primeiro que encontrar
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
-        <h1>Faça seu cadastro</h1>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="password"
-        />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="password"
+          />
 
-        <Button type="submit">Cadastrar</Button>
-      </Form>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para login
-      </a>
-    </Content>
-  </Container>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para login
+        </a>
+      </Content>
+    </Container>
   );
 };
 
